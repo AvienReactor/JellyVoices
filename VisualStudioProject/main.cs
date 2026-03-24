c45d55047496c6a531a8df1e0081872b72daaef1
Jared Ponce <rexrapterking@gmail.com>
2026-03-24 01:42:33 -0700

    wiki images

--- VisualStudioProject/main.cs
+++ VisualStudioProject/main.cs
@@ -29,16 +29,6 @@ public class JellyVoices(
     WTTServerCommonLib.WTTServerCommonLib wttCommon
 ) : IOnLoad
 {
-    internal static readonly Dictionary<string, string> CharacterAudioMap = new()
-    {
-        {"Jerma", "jerma"}
-    };
-
-    //internal static readonly List<string> AudioBundleKeys = new()
-    //    {
-    //        "audio/jelly.bundle"
-    //    };
-
     public async Task OnLoad()
     {
 
@@ -47,15 +37,8 @@ public class JellyVoices(
 
 
         // Use WTT-CommonLib services
-        //await wttCommon.CustomItemServiceExtended.CreateCustomItems(assembly);
-        //await wttCommon.CustomLocaleService.CreateCustomLocales(assembly);
-
         await wttCommon.CustomVoiceService.CreateCustomVoices(assembly);
-        //wttCommon.CustomAudioService.RegisterAudioBundles(AudioBundleKeys);
-        foreach (var kvp in CharacterAudioMap)
-        {
-            wttCommon.CustomAudioService.CreateFaceCardAudio(kvp.Key, kvp.Value);
-        }
+
         await Task.CompletedTask;
     }
 }
